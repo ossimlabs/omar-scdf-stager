be733a20817a88294c49e066990befd5d4131361
slallier <stephen.lallier@digitalglobe.com>
2017-06-09 11:00:14 -0400

    removed final modifier from class

--- src/main/groovy/io/ossim/omar/scdf/stager/OmarScdfStagerApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/stager/OmarScdfStagerApplication.groovy
@@ -17,7 +17,7 @@ import org.springframework.messaging.handler.annotation.SendTo
  */
 @SpringBootApplication
 @EnableBinding(Processor.class)
-final class OmarScdfStagerApplication {
+class OmarScdfStagerApplication {
 
     /**
      * The application logger
