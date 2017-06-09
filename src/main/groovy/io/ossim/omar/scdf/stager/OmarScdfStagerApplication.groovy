cb72a816011978769e7139fa806431bb6c24b498
slallier <stephen.lallier@digitalglobe.com>
2017-06-09 10:35:16 -0400

    changed to a processor in order to send a "successfully staged" message to a sink/listener

--- src/main/groovy/io/ossim/omar/scdf/stager/OmarScdfStagerApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/stager/OmarScdfStagerApplication.groovy
@@ -1,12 +1,14 @@
 package io.ossim.omar.scdf.stager
 
+import groovy.json.JsonBuilder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
-import org.springframework.cloud.stream.messaging.Sink
+import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.messaging.handler.annotation.SendTo
 
 /**
  * Created by slallier on 6/8/2017
@@ -14,18 +16,13 @@ import org.springframework.cloud.stream.messaging.Sink
  * The OmarScdfStagerApplication is a purpose built image stager for integration with a full SCDF stack.
  */
 @SpringBootApplication
-@EnableBinding(Sink.class)
+@EnableBinding(Processor.class)
 final class OmarScdfStagerApplication {
 
     /**
      * The application logger
      */
     private final Logger logger = LoggerFactory.getLogger(this.getClass())
-
-    /**
-     * The filename of the image to stage
-     */
-    private String imageFilename
 
     /**
      * Constructor
@@ -46,10 +43,16 @@ final class OmarScdfStagerApplication {
      * The callback for when a filename is received
      * @param message the body of the SQS message from the queue
      */
-    @StreamListener(Sink.INPUT)
-    public void receiveFilenameToStage(String filename) {
+    @StreamListener(Processor.INPUT)
+    @SendTo(Processor.OUTPUT)
+    String stageImage(String filename) {
+        JsonBuilder stagedFile
         logger.debug("Test")
-        imageFilename = filename
-        logger.info("Received file ${imageFilename} to stage")
+        logger.info("Received file ${filename} to stage")
+        // add raster, etc
+
+        stagedFile = new JsonBuilder()
+        stagedFile(filename : filename, stagedSuccessfully : true)
+        stagedFile.toPrettyString()
     }
 }
